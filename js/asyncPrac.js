803f347bfc5df9847e542fa0e2ca76afba5630a9
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-01 09:50:40 +0200

    async

---
+++ js/asyncPrac.js
@@ -0,0 +1,9 @@
+'use strict';
+console.log('asyncPrac.js file was loaded');
+
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+// su atskirom funkcijom parsiusti ir iskonsolinti naudojant async await
+// todos - pirmus 10
+// posts - pirmus 15
+// comments - pirmus 7
