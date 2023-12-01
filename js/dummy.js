cd51fe5da79e8d5b6b0eab939c8587f6309a0528
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-01 11:01:09 +0200

    posts dummy

---
+++ js/dummy.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log('dummy.js file was loaded');
+
+// parsisiusti postus is documentacija https://dummyjson.com/docs/posts
+
+// parisiusti paprastai su funkcija,
+// parsisiusti su async ir await
+// parsisiusti su pagalbine fn getData()
+
+// iskonsolinti sitame faile visus post
