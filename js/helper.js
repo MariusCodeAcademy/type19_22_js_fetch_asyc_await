b5d8db85bf74a4f968abcb096f850fb834c839a5
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-01 10:47:30 +0200

    get Data

---
+++ js/helper.js
@@ -0,0 +1,29 @@
+'use strict';
+console.log('santas little helper.js file was loaded');
+
+// generic pagalbine funkcija kuri parsius duomenis ir grazins
+
+function getData(url) {
+  return fetch(url)
+    .then((resp) => {
+      console.log('resp ===', resp);
+      if (resp.ok === false) {
+        throw new Error(`Klaida: ${resp.statusText} code: ${resp.status}`);
+      }
+      return resp.json();
+    })
+    .then((data) => {
+      console.log('getData got data ===', data);
+      return data;
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+}
+
+// let todosUrl = 'https://jsonplaceholder.typicode.com/todos';
+// // let rez = getData(todosUrl);
+// // console.log('rez ===', rez);
+// getData(todosUrl).then((todoArr) => {
+//   console.log('todoArr.slice(0, 10) ===', todoArr.slice(0, 10));
+// });
